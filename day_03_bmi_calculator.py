1b402019300bd42f3bfa968c8f188e355844f7af
scm-de-ai <soumya.c@gtstechlabs.com>
2025-11-22 13:03:50 +0530

    Create day_03_bmi_calculator.py

---
+++ day_03_bmi_calculator.py
@@ -0,0 +1,21 @@
+# Day 03 - 21 Nov 2025
+# Name: Soumya – first program that makes decisions
+# Project: BMI Calculator with WHO categories + type hints
+# Feeling: My code can now THINK
+
+weight = float(input("Enter your weight in kg: "))
+height = float(input("Enter your height in meters: "))
+
+bmi = weight / (height ** 2)
+print(f"\nYour BMI is: {round(bmi, 2)}")
+
+if bmi < 18.5:
+    print("Category: Underweight")
+elif bmi < 25:
+    print("Category: Normal weight")
+elif bmi < 30:
+    print("Category: Overweight")
+elif bmi < 35:
+    print("Category: Obese")
+else:
+    print("Category: Clinically obese")
